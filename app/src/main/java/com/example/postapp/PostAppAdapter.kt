3710c8b4c810c6bd102f41371d85d669120d9cf3
rifqi-luthfi <rluthfi711@gmail.com>
2021-07-15 15:25:13 +0700

    Adding UI - Post

---
+++ app/src/main/java/com/example/postapp/PostAppAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.postapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PostAppAdapter  : RecyclerView.Adapter<PostAppAdapter.PostAppHolder>() {
+    inner class  PostAppHolder(v: View) : RecyclerView.ViewHolder(v){
+        fun bind() {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAppHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PostAppHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
